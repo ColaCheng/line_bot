c4e94d92a6f6b4ee23ffb3d005cb16208f2c62b5
Cola Cheng <imlikedog@gmail.com>
2020-03-17 15:51:50 +0800

    Remove unnecessary comment out code.

--- lib/line_bot.ex
+++ lib/line_bot.ex
@@ -1,6 +1,4 @@
 defmodule LineBot do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -12,8 +10,6 @@ defmodule LineBot do
       worker(LineBot.CowboyServer, [])
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: LineBot.Supervisor]
     Supervisor.start_link(children, opts)
   end
